4ec7207f33033d9f618036518eb3863c3aea0edc
ThekoMK <nikolovski.dejvid@yahoo.com>
2023-06-16 08:04:55 +0200

    Delete button finished

--- main.js
+++ main.js
@@ -1,44 +1,70 @@
-const todos = []
+let todos = [];
 
 class ToDo {
-    constructor(title, status, id) {
+    constructor(id, title, status) {
+        this.id = id;
         this.title = title;
         this.status = status;
-        this.id = id;
     }
-    uniqueId() {
-        this.id = todos.length
+
+    setIsCompleted(completionStatus) {
+        this.status = completionStatus;
     }
 }
 
-const addTaskBtn = document.getElementById('addTaskBtn');
-const taskInput = document.getElementById('task');
-const addedTasks = document.getElementById('addedTasks');
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskInput = document.getElementById("task");
+const addedTasks = document.getElementById("addedTasks");
 
-const ul = document.createElement('ul')
+const ul = document.createElement("ul");
 addedTasks.append(ul);
 
-addTaskBtn.addEventListener('click', (e) => {
+function renderTasks(newToDo) {
+    const li = document.createElement("li");
+    const input = document.createElement("input");
+    const label = document.createElement("label");
+    const deleteButton = document.createElement('button');
+    
+    input.setAttribute("type", "checkbox");
+    label.setAttribute("for", newToDo.id);
+    
+    input.id = newToDo.id;
+    label.innerText = newToDo.title;
+    deleteButton.innerText = 'Delete';
+    
+    input.addEventListener("click", (e) => {
+        if (e.target.checked === true) {
+            label.style.textDecoration = 'line-through';
+            newToDo.setIsCompleted(true);
+        } else {
+            label.style.textDecoration = 'none';
+            newToDo.setIsCompleted(false);
+        }
+    });
+    
+    
+    deleteButton.addEventListener('click', (e) => {
+        li.remove();
+
+        const filteredList = todos.filter(task => task.id !== newToDo.id);
+        todos = filteredList;
+        
+        console.log(filteredList)
+    })
+    
+    ul.append(li);
+    li.append(input, label,deleteButton);
+}
+
+addTaskBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     const inputValue = taskInput.value;
+    const newTask = new ToDo(new Date().valueOf(), inputValue, false);
 
-    const newTask = new ToDo(inputValue)
-    
     todos.push(newTask);
-    newTask.uniqueId()
 
-    taskInput.value = '';
+    taskInput.value = "";
 
-    console.log(todos)
-
-    const li = document.createElement('li');
-    ul.append(li);
-    li.innerHTML = `<label for="${newTask.id}">${newTask.title}</label>
-                    <input type="checkbox" class="taskCheckBox" id="${newTask.id}">`
-
+    renderTasks(newTask)
 });
-
-const checkBoxes = document.querySelectorAll('.taskCheckBox');
-
-console.log(checkBoxes)
